1e28bec3db8a87ee5023bfa22341fd740dfd663d
PiesekChlebek <97040468+PiesekChlebek@users.noreply.github.com>
2023-07-14 23:59:45 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Socket.IO connection handler
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Handle incoming messages
+  socket.on('chat message', (message) => {
+    console.log('Message received:', message);
+
+    // Broadcast the message to all connected clients
+    io.emit('chat message', message);
+  });
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
